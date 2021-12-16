34d4a907191f49a5467a46222837b55e019c5516
Chris Bessent <lost.dragonist@gmail.com>
2021-12-15 17:13:17 -0700

    Fix an infinite loop in the log censoring
    If you replace "user" with "username" then the string
    will become "username" which becomes "usernamename" which
    becomes "usernamenamename" which becomes "usernamenamenamename"
    and so on.
    
    Also switch to using a boost function because it's so much better.

--- src/log.h
+++ src/log.h
@@ -10,6 +10,7 @@
 #include <QRect>
 #include <QColor>
 #include <fmt/format.h>
+#include <boost/algorithm/string.hpp>
 
 #include "dllimport.h"
 
@@ -122,17 +123,12 @@ void doLog(
 
     // check the blacklist
     for (const BlacklistEntry& entry : bl) {
-      std::string::iterator it = std::search(s.begin(), s.end(), entry.filter.begin(), entry.filter.end(), [](char a, char b) { return toupper(a) == toupper(b); });
-      while (it != s.end())
-      {
-        s.replace(it, it + (ptrdiff_t)entry.filter.length(), entry.replacement);
-        it = std::search(s.begin(), s.end(), entry.filter.begin(), entry.filter.end(), [](char a, char b) { return toupper(a) == toupper(b); });
-      }
+      boost::algorithm::ireplace_all(s, entry.filter, entry.replacement);
     }
   }
   catch(fmt::format_error&)
   {
-    s = "format error while logging";
+    s = "format error while logging"; 
     lv = Levels::Error;
   }
   catch(std::exception&)
